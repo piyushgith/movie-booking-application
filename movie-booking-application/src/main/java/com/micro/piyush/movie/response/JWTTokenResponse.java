60d9381633fae210ed42c2ef1af11ef0d7a2c9b8
piyushprasad <piyushprasad2007@gmail.com>
2025-08-31 20:29:15 +0530

    added response

---
+++ movie-booking-application/src/main/java/com/micro/piyush/movie/response/JWTTokenResponse.java
@@ -0,0 +1,15 @@
+package com.micro.piyush.movie.response;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class JWTTokenResponse {
+    private String token;
+    private String role;
+}
